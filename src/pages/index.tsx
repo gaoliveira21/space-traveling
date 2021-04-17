8be3c32fcf109f4893aa1a2a83a46aa04271d061
Gabriel Jose de Oliveira <gaoliveira211999@gmail.com>
2021-04-17 14:24:44 -0300

    add posts page styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,65 @@
 import Head from 'next/head'
 
+import styles from '@/src/styles/pages/home.module.scss'
+
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Posts | SpaceTraveling</title>
       </Head>
 
-      <h1>Hello</h1>
-    </div>
+      <div className={styles.container}>
+        <img className={styles.c__logo} src="/images/logo.svg" alt="SpaceTraveling" />
+
+        <ul className={styles.c__posts}>
+          <li className={styles.p__post}>
+            <a href="#!">
+              <strong className={styles.p__title}>Como utilizar Hooks</strong>
+              <p className={styles.p__description}>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div>
+                <time className={styles.p__date}>15 Mar 2021</time>
+                <span className={styles.p__author}>Gabriel Oliveira</span>
+              </div>
+            </a>
+          </li>
+
+          <li className={styles.p__post}>
+            <a href="#!">
+              <strong className={styles.p__title}>Como utilizar Hooks</strong>
+              <p className={styles.p__description}>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div>
+                <time className={styles.p__date}>15 Mar 2021</time>
+                <span className={styles.p__author}>Gabriel Oliveira</span>
+              </div>
+            </a>
+          </li>
+
+          <li className={styles.p__post}>
+            <a href="#!">
+              <strong className={styles.p__title}>Como utilizar Hooks</strong>
+              <p className={styles.p__description}>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div>
+                <time className={styles.p__date}>15 Mar 2021</time>
+                <span className={styles.p__author}>Gabriel Oliveira</span>
+              </div>
+            </a>
+          </li>
+
+          <li className={styles.p__post}>
+            <a href="#!">
+              <strong className={styles.p__title}>Como utilizar Hooks</strong>
+              <p className={styles.p__description}>Pensando em sincronização em vez de ciclos de vida.</p>
+              <div>
+                <time className={styles.p__date}>15 Mar 2021</time>
+                <span className={styles.p__author}>Gabriel Oliveira</span>
+              </div>
+            </a>
+          </li>
+        </ul>
+
+        <button type="button" className={styles.c__load_more}>Carregar mais posts</button>
+      </div>
+    </>
   )
 }
